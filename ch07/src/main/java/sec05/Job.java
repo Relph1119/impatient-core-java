de61cd5309589e643bc25bbb30f9df5331793c3a
Relph1119 <huruifeng1202@163.com>
2024-06-24 21:45:35 +0800

    学习第7章

---
+++ ch07/src/main/java/sec05/Job.java
@@ -0,0 +1,7 @@
+package sec05;
+
+public record Job(int priority, String description) implements Comparable<Job> {
+    public int compareTo(Job other) {
+        return priority - other.priority;
+    }
+}
